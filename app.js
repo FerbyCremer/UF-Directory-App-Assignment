a1e26608da36c06d577c821c65f913744a10ee4d
FerbyCremer <jennifercremer@gmail.com>
2017-09-04 16:35:47 -0400

    completed assignment, includes form validation for code and name, currently still displays detailedInfo when interacting with the search bar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* register the modules the application depends upon here*/
-angular.module('listings', ['code','name','coordinates','address']);
+angular.module('listings', []);
 
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('directoryApp', ['listings']);
--- listingController.js
+++ listingController.js
@@ -1,13 +1,37 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
+    $scope.showForm = false;
     $scope.detailedInfo =  undefined;
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.form = {};
+
+    $scope.addListing = function() {
+      $scope.listings.push($scope.form);
+      $scope.form = {};
+      $scope.showForm = !$scope.showForm;
+    }
+
+    $scope.deleteListing = function(entry) {
+        let newListings = [];  //copy over list of buildings into new array while filtering out the ones to delete
+        newListings = $scope.listings.filter(function (listing) {
+          let deleteName = entry.name;
+          let deleteCode = entry.code;
+          if ( listing.name !== deleteName && listing.code !== deleteCode) {
+            return listing;
+          }
+        });
+        //then replace listings with newListings
+        $scope.listings = newListings;
+        //delete related detailedInfo
+        $scope.detailedInfo = undefined;
+    }
+
+    $scope.showDetails = function(entry) {
+      $scope.detailedInfo = entry;
+    }
+
+    $scope.changeForm = function () {
+      $scope.showForm = !$scope.showForm;
+    }
   }
 ]);
