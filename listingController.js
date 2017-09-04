65a0d448cafb4fa3358a08d7df958bdd7e5b00a1
FerbyCremer <jennifercremer@gmail.com>
2017-09-03 22:01:43 -0400

    listings

--- listingController.js
+++ listingController.js
@@ -1,8 +1,7 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
-
+    $scope.detailedInfo =  undefined;
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
